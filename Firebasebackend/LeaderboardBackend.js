52777aacf3de05d38cf9768957b400866ad4c847
weijie <ongweijie7@gmail.com>
2022-07-11 21:11:18 +0800

    Sync firebase with leaderboard and included add friends function

---
+++ Firebasebackend/LeaderboardBackend.js
@@ -0,0 +1,42 @@
+import { firebaseApp } from "../firebase.config";
+import { doc, onSnapshot, query, where, collection, getDocs, updateDoc, getDoc } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+import global from '../global';
+
+//initialising database
+const db = getFirestore(firebaseApp);
+
+//query for all user that have currentuser as a friend and then push their latest update into the leaderboard list
+
+{/*Detect whenever there are changes*/}
+const trackChanges = onSnapshot(doc(db, "Users", "ongweijie7@gmail.com"), (snapshot) => {
+    console.log("changes are made");
+});
+
+{/*Query for a valid user and then add it to the current user's friend arr*/}
+const addUser = (email) => {
+    const docref = doc(db, "Users", email);
+    getDoc(docref).then((snapshot) => {
+        if (snapshot.exists()) {
+            const arr = snapshot.data().friends;
+            const arr1 = global.friends;
+            arr.push(global.email);
+            arr1.push(email);
+            //updates the
+            updateDoc(docref, {
+                friends: arr,
+            })
+            updateDoc(doc(db, "Users", global.email), {
+                friends: arr1,
+            })
+        } else {
+            alert("User does not seem to exist! Please enter a valid user email");
+        }
+        
+    }).catch((error) => {
+        console.log(error.message);
+    })
+
+}
+
+export { trackChanges, addUser };
---
+++ Firebasebackend/LoginBackend.js
@@ -0,0 +1,23 @@
+import { firebaseApp } from "../firebase.config";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+import global from '../global';
+
+
+//initialising database
+const db = getFirestore(firebaseApp);
+
+export default async function SignIn(email) { 
+    const docref = doc(db, "Users", email);
+    getDoc(docref).then(docSnap => {
+        if (docSnap.exists()) {
+            global.email = email;
+            global.username = docSnap.data().name;
+            global.coins = docSnap.data().coins;
+            global.leaderboard = docSnap.data().leaderboard;
+        } else {
+            console.log("no such document")
+        }
+    })
+    
+}
